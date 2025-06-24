e806a5f8d460af8f2efdd1fa9d24ee794810d2d8
Make It Look <info@makeitlook.co.uk>
2025-06-24 14:02:14 +0100

    feat: add fullscreen hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,23 @@ import AnimatedDiv from "@/components/AnimatedDiv/AnimatedDiv";
 import AnimatedGradientBackground from "@/components/GradientBackground/AnimatedGradientBackground";
 import RestaurantInfo from "@/components/InfoCard/InfoCard";
 import PageLayout from "@/components/Layouts/PageLayout";
+import HeroSection from "@/components/HeroSection/HeroSection";
 
 export default function Home() {
   return (
-    // <AnimatedGradientBackground>
-    <PageLayout title="Home">
-      <section id="home">
-        <AnimatedDiv>
-          We`&apos;`d love to hear from you! Drop us a message and let`&apos;`s
-          create something beautiful together.
-        </AnimatedDiv>
-      </section>
-      <RestaurantInfo />
-    </PageLayout>
-    // </AnimatedGradientBackground>
+    <>
+      <HeroSection />
+      {/* <AnimatedGradientBackground> */}
+      <PageLayout title="Home">
+        <section id="home">
+          <AnimatedDiv>
+            We`&apos;`d love to hear from you! Drop us a message and let`&apos;`s
+            create something beautiful together.
+          </AnimatedDiv>
+        </section>
+        <RestaurantInfo />
+      </PageLayout>
+      {/* </AnimatedGradientBackground> */}
+    </>
   );
 }
