ed04226d5d505127ea0aae87040a509ffc7e92d3
Make It Look <info@makeitlook.co.uk>
2025-07-01 18:05:33 +0100

    Ensure mobile nav stops page scrolling

--- src/components/Navbar/ConfigurableNav/ConfigurableNav.tsx
+++ src/components/Navbar/ConfigurableNav/ConfigurableNav.tsx
@@ -149,11 +149,14 @@ const ConfigurableNavigation: React.FC<NavProps> = ({
   useEffect(() => {
     if (mobileMenuOpen) {
       document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
     }
     return () => {
       document.body.style.overflow = "";
+      document.documentElement.style.overflow = "";
     };
   }, [mobileMenuOpen]);
 
